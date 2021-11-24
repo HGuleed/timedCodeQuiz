e06c13d8e588d3ecb22206266f5e2cac1c724c00
Hodan <you@example.com>
2021-11-24 09:11:09 -0500

    added questions and answers to array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,41 @@
 var questionArray = [{
-    question:
-    optionsList: [optionsA, optionsB, optionC,optionD]
-    answer: optionA
+    question: "What does SEO stand for?",
+    optionsList: ["Source Engine Organization", "Social Engineer Operator", "Search Engine Optimization","Software Engineer Operator"],
+    answer: "Search Engine Optimization"
 },{
-    question:
-    optionsList;[optionsA, optionsB, optionC,optionD]
-    answer:
-}]
+    question: "What is the order for labeling margin and padding?",
+    optionsList:["LBRT", "TRBL", "TBRL","BTLR"],
+    answer: "TRBL"
+},{
+    question:"Which one is NOT a content model?",
+    optionsList:["heading", "sectioning", "iframe","flow"],
+    answer: "iframe"
+},{
+    question:"What is used to select an element and apply style to it?",
+    optionsList: ["selector", "class", "media query","inline"],
+    answer: "selector"
+},{
+    question: "How does z-index affect a web page?",
+    optionsList:["let's you control main axis", "makes it responsive", "let's you control the cross axis", "allows you to control the depth of objects"],
+    answer: "allows you to control the depth of objects"
+},{
+    question: "What is the difference between an element and and attribute?",
+    optionsList;["an element is any tag on your page, an attribute provides details about an element", "an attribute is any tag on your page, an element provides details about an attribute", " element are found in HTML, attributes are found in CSS"," attributes are found in HTML, element are found in CSS"],
+    answer: "an element is any tag on your page, an attribute provides details about an element"
+},{
+    question:"What are the 3 main breakpoints?",
+    optionsList:["600px, 800px, 1000px", "560px, 820px, 975px", "575px, 768px, 980px","500px, 700px, 900px"],
+    answer:"575px, 768px, 980px"
+},{
+    question: "What is a code block?",
+    optionsList:["code on HTML", "code between curly braces", "code in blocks","code between quotations"],
+    answer:"code between curly braces"
+},{
+    question: "What is a plain language description of the steps that an algorithm or application must complete?",
+    optionsList:["expression", "condition", "declaration","pseudocode"],
+    answer: "pseudocode"
+},{
+    question: "What is a data structure designed to store data?",
+    optionsList:["array", "object", "primitive data","arguments"],
+    answer: "array"
+}];
