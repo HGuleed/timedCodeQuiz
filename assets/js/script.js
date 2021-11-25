a4ee5615e1c18c79c3946b8beb04d714e7bb1cab
Hodan <you@example.com>
2021-11-25 11:41:02 -0500

    fixed errors, first question populates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,43 @@
 var index = 0;
+var questionIndex = 0;
 var currentTime = 75000;
 var currentScore = 0;
-
-function startQuiz() {
-  askQuestion();
-  initiateTimer();
-}
-document.getElementById("generate").addEventListener("mousedown", startQuiz);
+var currentQuestion = questionArray[index].question;
+var 
 
 function askQuestion() {
-  var currentQuestion = questionArray[index];
-  document.getElementById("quizQuestions").textContent = getQuestion.question;
+  debugger;
+  // var optionA = questionArray[index].optionsList[0];
+  // var optionB = questionArray[index].optionsList[1];
+  // var optionC = questionArray[index].optionsList[2];
+  // var optionD = questionArray[index].optionsList[3];
+  // var startButton = document.getElementById("startBtn");
+  // var options = document.createElement("li");
+  // var populateOp = document.createTextNode();
+  document.getElementById("title").textContent = "";
 
+  document.getElementById("intro").textContent = "";
+
+  for (let i = index; i < questionArray.length; i++) {
+    document.getElementById("quizQuestions").textContent = currentQuestion;
+  }
+  console.log(currentQuestion);
 }
-function checksAnswer(event) {
-  var rightAns= questionArray[index].answer;
-  if ( ==rightAns )
 
+// function checksAnswer(event) {
+//   var rightAns= questionArray[index].answer;
+
+// }
+
+// function initiateTimer() {
+
+// }
+
+// function
+
+function startQuiz() {
+  debugger;
+  askQuestion();
+  // initiateTimer();
 }
-
-function initiateTimer() {
-
-}
-
-function 
+document.getElementById("generate").addEventListener("mousedown", askQuestion);
